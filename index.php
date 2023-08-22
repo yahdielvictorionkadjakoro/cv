8bc72116a066254483f7e72250788931753c3764
yahdielvictorionkadjakoro <113011048+yahdielvictorionkadjakoro@users.noreply.github.com>
2023-08-22 11:10:20 +0700

    Add files via upload
    Signed-off-by: yahdielvictorionkadjakoro <113011048+yahdielvictorionkadjakoro@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<section>
+		<h1>Selamat datang di aplikasi Catatan Perjalanan</h1>
+		<h4>Silahkan Masukkan Nama lengkap dan NIK anda</h4>
+	</section>
+	<form name="login" action="#" method="post">
+		<ul>
+			<li>
+				<label for="nama">Nama</label>
+				<input type="text" name="nama" placeholder="Nama" required>
+			</li>
+			<li>
+				<label for="nama">Nama</label>
+				<input type="text" name="nik" placeholder="NIK" required>
+			</li>
+			<li>
+				<button><a href="daftar.php">Saya Pengguna Baru</a></button>
+				<input type="submit" value="MASUK">
+			</li>
+		</ul>
+	</form>
+</body>
+</html>
